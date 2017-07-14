13631c8bb40cc7669db87bf7cacfd6f1925fe055
ilduchea <ilduchea@gmail.com>
2017-07-14 13:52:41 -0700

    add sample images to the db seed. add image display to product details.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,16 @@ class Seed
 
   def generate_products
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      i = rand(1..3)
+      Product.create!(
+        name: Faker::Lorem.word,
+        price: rand(10...100),
+        description: Faker::Lorem.sentence(5, false, 0).chop,
+        image: File.new("app/assets/images/image#{i}.jpg")
+      )
     end
   end
-  
+
   def create_users
     1.times do |index1|
       User.create!(
