7e24f9bf0d85ebb9f3e4f3d491374b6a175abca8
ilduchea <ilduchea@gmail.com>
2017-07-14 09:04:21 -0700

    add schema testing for Users. Passing.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # it { should validate_presence_of :email }
+  it { should have_secure_password }
+  it { should have_many :orders }
   # it { should validate_uniqueness_of :email }
   # it { should validate_confirmation_of :password }
   #
