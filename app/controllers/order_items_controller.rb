10db60716685c0827708b772218c656cb9384e05
ilduchea <ilduchea@gmail.com>
2017-07-14 14:29:56 -0700

    add ajax to cart item delete.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -25,7 +25,9 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.find(params[:id])
     @item.destroy
     @order.save
-    redirect_to cart_path
+    respond_to do |f|
+      f.js { render 'carts/destroy.js.erb' }
+    end
   end
 
   private
